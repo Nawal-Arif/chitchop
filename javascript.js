0c8cedcda7a219ca2ac14d87ba7db80ed4ac4a64
Romi Arif Malik <99647886+Romi-Arif@users.noreply.github.com>
2023-08-24 22:32:54 +0500

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -2,21 +2,22 @@ var picNextBtn = document.querySelector(".pic .next");
 var picPrevBtn = document.querySelector(".pic .previous");
 
 var img = document.querySelector(".pic img");
-var i=3;
+var i=1;
 var totalPic=3;
 var imgSrc = i+".jpg";
 
 // pic Next button click event//
 picNextBtn.addEventListener("click",function(){
-    i+=1;
-    if(i==totalPic){
-        img.setAttribute("src","2.jpg");
+    
+    if(i==1){
+        img.setAttribute("src","1.jpg");
         i=2;
     }else if(i==2){
-        img.setAttribute("src","1.jpg");
-        i=1
+        img.setAttribute("src","2.jpg");
+        i=3;
     }else{
         img.setAttribute("src","3.jpg");
+        i=1;
     }
 });
 
